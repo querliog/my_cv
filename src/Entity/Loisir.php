17f53b3375362f401c44454860491971b0bfdb6c
Gary Querlioz <gary.querlioz@etu.univ-grenoble-alpes.fr>
2019-02-18 10:06:50 +0000

    Début jour 4

--- src/Entity/Loisir.php
+++ src/Entity/Loisir.php
@@ -16,8 +16,42 @@ class Loisir
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $annee;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?string
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(string $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
     }
 }
